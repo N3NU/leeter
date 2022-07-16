c05622f38c1b8438828773f76606f764cc20aa20
N3NU <stevencaraballo27@gmail.com>
2022-07-15 23:03:06 -0400

    Added If statement to check if a character is a letter or not. Got things working. Only printing one word now password --> p455w02d

--- leeter.sh
+++ leeter.sh
@@ -10,6 +10,7 @@ declare -A dict=( \
 [O]=0 [o]=0 [P]=p [p]=p [Q]=q [q]=q [R]=2 [r]=2 [S]=5 [s]=5 [T]=7 [t]=7 [U]=u [u]=u \
 [V]=v [v]=v [W]=w [w]=w [X]=x [x]=x [Y]=y [y]=y [Z]=2 [z]=2 ) 
 
+new_word=""
 #48cd3f9h1jk1mn0pq257uvwxy2
 #user prompt
 read -p "Please enter word to leet: " user_input
@@ -18,5 +19,11 @@ read -p "Please enter word to leet: " user_input
 #code
 for i in $(grep -o . <<< "$user_input")
 do
-	echo $i
+	if [[ $i == [a-zA-Z] ]]
+	then 
+		new_word=${new_word}${dict[${i}]}
+	else
+		new_word=${new_word}${dict[${i}]}
+	fi
 done
+echo $new_word
